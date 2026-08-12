4059206003c3c3f2dc7ad100eb6525b604146ef6
BragaSoterasVinicius <viniciussoteras@gmail.com>
2026-08-12 20:39:02 -0300

    feat: exercicio 1

---
+++ ex1.dart
@@ -0,0 +1,11 @@
+final int id = 1;
+final String nome = 'Vinicius Soteras Braga';
+final String cpf = '789521568521';
+final String email = 'viniciussoteras@gmail.com';
+final String telefone = '(11) 91111-1111';
+
+void main() {
+  print('Paciente: #$id - $nome');
+  print('CPF: | $cpf');
+  print('Contato: $email| $telefone');
+}
